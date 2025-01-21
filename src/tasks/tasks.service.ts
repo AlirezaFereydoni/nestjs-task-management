763c7f1871f184ea2ba95124efac9b9c3f6b2207
Alireza Fereydoni <Aliirzw@gmail.com>
2025-01-21 10:59:15 +0330

    feat: add get and post method for tasks

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { randomUUID } from 'crypto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string) {
+    const task: Task = {
+      id: randomUUID(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+
+    return task;
+  }
+}
